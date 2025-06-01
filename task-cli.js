756474bf7c182aee9cc9fe13896e3ab5df3916db
Mujtaba-abd <mujataba2abd@gmail.com>
2025-06-01 23:18:56 +0300

    figerd out how to handel user input

--- task-cli.js
+++ task-cli.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
+// import {argv} from "node:process"
+const argv = require("node:process")
 
 function main(){
     console.log("hello this is a cli app")
+    console.log(process.argv[2])
 }
 
 main()
